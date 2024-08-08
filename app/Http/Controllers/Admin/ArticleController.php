fe371d2701b19c817c43bca3fa74551dec2a5c8f
Ceyda Tak <ceydatak03@gmail.com>
2024-08-08 19:39:27 +0300

    middleware eklendi

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers;
 
-class ArticleController extends Controller
+class ArticleController extends \Illuminate\Routing\Controller
 {
+    public function __construct()
+    {
+        $this->middleware("language");
+    }
+
     public function index()
     {
         return view('admin.articles.list');
@@ -14,6 +20,7 @@ class ArticleController extends Controller
 
     public function create()
     {
+        dd(app()->getLocale());
         return view('admin.articles.create-update');
     }
 }
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            "language" => \App\Http\Middleware\Language::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
